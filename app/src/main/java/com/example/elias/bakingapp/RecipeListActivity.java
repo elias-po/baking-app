dd95d0cd76213593eb237b148178c4a948db5f94
elias-po <thendbims@gmail.com>
2018-07-17 16:16:30 +0300

    added grid recipe view for tablets

--- app/src/main/java/com/example/elias/bakingapp/RecipeListActivity.java
+++ app/src/main/java/com/example/elias/bakingapp/RecipeListActivity.java
@@ -2,8 +2,10 @@ package com.example.elias.bakingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,7 +23,12 @@ public class RecipeListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recipe_list);
         ButterKnife.bind(this);
 
-        recipeListLayoutManager = new LinearLayoutManager(this);
+        LinearLayout recipe_list_wrapper = findViewById(R.id.recipe_list_wrapper);
+        if (recipe_list_wrapper == null) {
+            recipeListLayoutManager = new LinearLayoutManager(this);
+        } else {
+            recipeListLayoutManager = new GridLayoutManager(this, 3);
+        }
         rv_recipeList.setLayoutManager(recipeListLayoutManager);
     }
 
